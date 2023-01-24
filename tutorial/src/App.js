15bbfd261b9a505deaa8309e78f8a2dbdf524da5
serifcolakel <serifcolakel0@gmail.com>
2023-01-24 19:49:57 +0300

    Test commit

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,5 +1,6 @@
 // src klasörü altundaki logo.svg import etmek gerekiyor
-import logo from "./logo.svg";
+import PropsTutorialPage from "./tutorials/PropsTutorialFromPrototurk";
+import StateTutorialFromPrototurk from "./tutorials/StateTutorialFromPrototurk";
 
 function App() {
   return (
@@ -8,12 +9,14 @@ function App() {
       statik olarak görseli çekmek için. 
       kalıcı bir resim kalacaksa public klasörü içerinde kullanımalı ve çağrılmalıdır. 
       */}
-      <img src="./logo192.png"></img>
+      <h1>PropsTutorialFromPrototurk</h1>
+      <PropsTutorialPage />
+      <h1>PropsTutorialFromPrototurk</h1>
+      <StateTutorialFromPrototurk />
       {/* src klassörü altındaki logo ya erişim sağlamak için
       <img src="./logo.svg"></img>  bu şekilde kullanım olmuyor  */}
       {/* src klasörü veya public klasörü içerinde çağrılan verilen her zaman değişikliğe uğrayabilir. */}
       {/* import edilen dosyayı çağırmak için */}
-      <img src={logo}></img>
       {/* package.json içerisindeki
        verilen bilgileri source kısımda açık olaark görülmemesi için // build
       kısmına GENERATE_SOURCEMAP = false */}
---
+++ tutorial/src/tutorials/StateTutorialFromPrototurk/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UseReducerHook from "./components/UseReducerHook";
+
+export default function StateTutorialFromPrototurk() {
+  return (
+    <div>
+      StateTutorialFromPrototurk
+      <UseReducerHook />
+    </div>
+  );
+}
